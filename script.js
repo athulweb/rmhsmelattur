150eece5131072dcc60d157ad068b4e775430e46
Athul vinod .M <muringanattuppara@gmail.com>
2025-06-18 21:30:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const sidebar = document.getElementById("mySidebar");
 const toggleBtn = document.getElementById("sidebarToggle");
 
 toggleBtn.onclick = () => {
-  const currentLeft = window.getComputedStyle(sidebar).left;
-  sidebar.style.left = currentLeft === "0px" ? "-250px" : "0px";
+  const computedLeft = window.getComputedStyle(sidebar).left;
+  sidebar.style.left = (computedLeft === "0px") ? "-250px" : "0px";
 };
