e0f794073fa4188aa92dadab80901b6bdda9ede1
Athul vinod .M <muringanattuppara@gmail.com>
2025-06-18 20:46:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const toggleBtn = document.getElementById("sidebarToggle");
 toggleBtn.onclick = () => {
   sidebar.style.left = sidebar.style.left === "0px" ? "-250px" : "0px";
 };
+
+
+
+function updateTime() {
+  const now = new Date();
+  const time = now.toLocaleTimeString('ml-IN');
+  const date = now.toLocaleDateString('ml-IN');
+  document.getElementById("clock").innerText = `ഇന്നത്തെ തീയതി: ${date}\nസമയം: ${time}`;
+}
+setInterval(updateTime, 1000);
+updateTime();
